7bc90102b8e338b62174e4adfc5b051ac588ea05
mohamedd2003 <105282528+mohamedd2003@users.noreply.github.com>
2024-04-25 22:56:29 +0200

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -10,24 +10,25 @@ export default function Navbar() {
    <>
    <nav className="navbar navbar-expand-lg  navbar-dark fixed-top  z-2">
   <div className="container-fluid px-5 py-1">
-  <img src={logo} alt="TRT-Log" className={`  ${style.logo}`} />
+  
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav ms-auto  ">
+      <ul className="navbar-nav m-auto  ">
         <li className="nav-item mt-3 ">
           <Link className={` nav-link text-center  active ${style.varelaRegular}`}  aria-current="page" to='/home'>Home</Link>
         </li>
-    <Link className="navbar-brand mx-2 " to='home'>
-
-    
-
-
-    </Link>
+   
         <li className="nav-item mt-3 ">
           <Link className={` nav-link text-center   ${style.varelaRegular}`} to='/about'>About</Link>
         </li>
+         <Link className="navbar-brand mx-2 " to='home'>
+
+    <img src={logo} alt="TRT-Log" className={`  ${style.logo}`} />
+
+
+    </Link>
     
         <li className="nav-item mt-3 ">
           <Link className={` nav-link text-center   ${style.varelaRegular}`} to='/contact'>Contact</Link>
