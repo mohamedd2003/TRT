c487081ba93656311391ea19074fbebe4ceca386
mohamedd2003 <105282528+mohamedd2003@users.noreply.github.com>
2024-04-27 01:47:01 +0300

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import style from './Home.module.css'
+import './Home.module.css';
+
+//Framer-Motion library Imports
 import { motion } from 'framer-motion'
+
+//Swiper library Imports
+import 'swiper/css/effect-creative';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
-import './Home.module.css';
-import { Pagination, Autoplay,Navigation  } from 'swiper/modules';
+import { Pagination, Autoplay,Navigation , EffectCreative  } from 'swiper/modules';
 import 'swiper/css/navigation';
+// image imports
 import slide1 from '../../assets/Images/slide1.jpg'
 import slide2 from '../../assets/Images/slide2.jpg'
 import slide3 from '../../assets/Images/slide3.jpg'
@@ -14,6 +20,9 @@ import slide4 from '../../assets/Images/slide4.jpg'
 import slide5 from '../../assets/Images/slide5.jpg'
 import slide6 from '../../assets/Images/slide6.jpg'
 import slide7 from '../../assets/Images/slide7.jpg'
+import slide8 from '../../assets/Images/slide8.jpg'
+import TRT from '../../assets/Images/TRT.jpg'
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   
@@ -32,6 +41,17 @@ export default function Home() {
 
         
       <Swiper
+          grabCursor={true}
+          effect={'creative'}
+          creativeEffect={{
+            prev: {
+              shadow: true,
+              translate: [0, 0, -400],
+            },
+            next: {
+              translate: ['100%', 0, 0],
+            },
+          }}
         className="mySwiper vh-100 "
         animating={true}
         autoplay={{
@@ -40,10 +60,8 @@ export default function Home() {
         }}
         navigation={true}
         pagination={pagination}
-        modules={[Pagination,Autoplay,Navigation]}
-      >
-        <SwiperSlide  
-   className=' position-relative '>
+        modules={[Pagination,Autoplay,Navigation,EffectCreative]}>
+        <SwiperSlide  className=' position-relative '>
           <img src={slide1} alt="Opel Astra " className='w-100' />
 
           <div className={`${style.overlay} d-flex  align-items-center justify-content-center  `}>
@@ -75,12 +93,25 @@ export default function Home() {
         </SwiperSlide>
         <SwiperSlide>
           <Swiper
+         grabCursor={true}
+         effect={'creative'}
+         creativeEffect={{
+           prev: {
+             shadow: true,
+             translate: [0, 0, -800],
+             rotate: [180, 0, 0],
+           },
+           next: {
+             shadow: true,
+             translate: [0, 0, -800],
+             rotate: [-180, 0, 0],
+           },
+         }}
             className="mySwiper2 swiper-v"
             direction={'vertical'}
             spaceBetween={50}
             pagination={pagination}
-            modules={[Pagination]}
-          >
+            modules={[Pagination,EffectCreative]}>
             <SwiperSlide className='position-relative '>
               <img src={slide2} alt="Dodge Car" className='w-100' />
 
@@ -120,12 +151,40 @@ export default function Home() {
           <div className={`${style.overlay}`}></div>
 
         </SwiperSlide>
+
+        <SwiperSlide className=' position-relative '>
+          <img src={slide8} alt="BMW Motocycle" className='w-100' />
+
+          <div className={`${style.overlay}`}></div>
+
+        </SwiperSlide>
         
       </Swiper>
       </section>
 
-      <section id='about'>
-        
+      <section id='about' className='mt-5'>
+        <div className="container">
+          <div className="row mt-5 gx-5">
+            <div className="col-lg-6 "   >
+              <div className="vid">
+                <img src={TRT} alt="Elshrouk Academy" className='w-100 rounded rounded-4' />
+                <Link className='btn btn-outline-danger rounded-end-pill mt-3 m-auto h2 w-100 ' to='/about'>Learn More</Link>
+              </div>
+            </div>
+            <div className="col-lg-6 ">
+              <div className="breif">
+<span className='text-muted ms-lg-5 h5'><i className="fa-solid fa-t fa-sm"></i><i className="fa-brands  fa-r-project fa-sm my-5"></i><i className="fa-solid fa-t fa-sm"></i></span>
+<h3 className='main-font h1   mb-4 text-muted '>ABOUT U<i className="fa-solid fa-bolt fa-md text-main"></i></h3>
+
+<p className='text-muted ms-lg-5'><span className='text-main'>  TRT </span> exhibition organizing team was established in <span className='text-main'>2022</span> , where we organized our first event on <span className='text-main'>2022</span>, 
+and it was under a graduation project for media students of the Public Relations Department. Many influencers in the field of cars on social media attended, and many companies specializing in car modification attended.
+We participated in organizing an event for modified cars at one of the universities in the Administrative Capital, and this is an indication of our success in this work.
+Our Mission : is to continue organizing outstanding events that cater to the interests of car enthusiasts and owners of modified cars, providing a platform for them to showcase their passion and connect with like-minded individuals. We are committed to upholding the highest standards of professionalism and creating a safe and enjoyable environment for all participants. With a focus on innovation and creativity, we aim to set new benchmarks in the industry and establish ourselves as a leader in event organizing for car enthusiasts in the Arab Republic of Egypt. "Make your passion safe" is not just a slogan for us, but a guiding principle that drives everything we do.
+"Make your passion safe"</p>
+              </div>
+            </div>
+          </div>
+        </div>
          </section>
     </>
   )
