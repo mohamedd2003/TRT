3fc5b775450b78d4f42adef97afe874943279b6a
mohamedd2003 <105282528+mohamedd2003@users.noreply.github.com>
2024-07-06 15:13:10 +0300

    Add files via upload

--- src/Components/Images/Images.js
+++ src/Components/Images/Images.js
@@ -1,103 +1,102 @@
 // imageImports.js
 
 //**************RED IMAGES**************************************
-import BMW1 from "../../assets/Images/red/BMW/1.jpeg";
-import BMW3 from "../../assets/Images/red/BMW/3.jpg";
-import BMW4 from "../../assets/Images/red/BMW/4.jpg";
-import E92 from "../../assets/Images/red/BMW/E92.jpg";
-import E921 from "../../assets/Images/red/BMW/E921.jpg";
-import E922 from "../../assets/Images/red/BMW/E922.jpg";
-import MOTO from "../../assets/Images/red/BMW/motorcycle01.jpg";
-import MOTO2 from "../../assets/Images/red/BMW/motorcycle02.jpg";
-import porscha from "../../assets/Images/red/porscha.jpeg";
-import seat from "../../assets/Images/red/8.jpg";
-import skoda from "../../assets/Images/red/SKODA/7.jpg";
-import honda1 from "../../assets/Images/red/HONDA/honda1.jpg";
-import honda2 from "../../assets/Images/red/HONDA/honda2.jpg";
-import honda3 from "../../assets/Images/red/HONDA/honda3.jpg";
-import puma from "../../assets/Images/red/MITSUBISHI/7.jpg";
-import khonfsa from "../../assets/Images/red/VW/khonfsa.jpg";
-import skoda1 from "../../assets/Images/red/SKODA/skoda1.jpg";
-import skoda2 from "../../assets/Images/red/SKODA/skoda2.jpg";
-import skodaa7 from "../../assets/Images/red/SKODA/skoda.jpg";
-import insgnia1 from "../../assets/Images/red/OPEL/1.jpg";
-import insgnia2 from "../../assets/Images/red/OPEL/2.jpg";
-import insgnia3 from "../../assets/Images/red/OPEL/3.jpg";
-import insgnia4 from "../../assets/Images/red/OPEL/opel.jpg";
+import BMW1 from "../../assets/Images/red/BMW/1.webp";
+import BMW3 from "../../assets/Images/red/BMW/3.webp";
+import BMW4 from "../../assets/Images/red/BMW/4.webp";
+import E92 from "../../assets/Images/red/BMW/E92.webp";
+import E921 from "../../assets/Images/red/BMW/E921.webp";
+import E922 from "../../assets/Images/red/BMW/E922.webp";
+import MOTO from "../../assets/Images/red/BMW/motorcycle01.webp";
+import porscha from "../../assets/Images/red/porscha.webp";
+import seat from "../../assets/Images/red/8.webp";
+import skoda from "../../assets/Images/red/SKODA/7.webp";
+import honda1 from "../../assets/Images/red/HONDA/honda1.webp";
+import honda2 from "../../assets/Images/red/HONDA/honda2.webp";
+import honda3 from "../../assets/Images/red/HONDA/honda3.webp";
+import puma from "../../assets/Images/red/MITSUBISHI/7.webp";
+import khonfsa from "../../assets/Images/red/VW/khonfsa.webp";
+import skoda1 from "../../assets/Images/red/SKODA/skoda1.webp";
+import skoda2 from "../../assets/Images/red/SKODA/skoda2.webp";
+import skodaa7 from "../../assets/Images/red/SKODA/skoda.webp";
+import insgnia1 from "../../assets/Images/red/OPEL/1.webp";
+import insgnia2 from "../../assets/Images/red/OPEL/2.webp";
+import insgnia3 from "../../assets/Images/red/OPEL/3.webp";
+import insgnia4 from "../../assets/Images/red/OPEL/opel.webp";
 
 
 //**************ORANGE IMAGES**************************************
-import opel1 from "../../assets/Images/orange/OPEL/1.jpg";
-import opel2 from "../../assets/Images/orange/OPEL/2.jpg";
-import opel3 from "../../assets/Images/orange/OPEL/3.jpg";
-import opel4 from "../../assets/Images/orange/OPEL/4.jpg";
-import opel5 from "../../assets/Images/orange/OPEL/5.jpg";
-import opel6 from "../../assets/Images/orange/OPEL/6.jpg";
-import opel7 from "../../assets/Images/orange/OPEL/7.jpg";
-import opel8 from "../../assets/Images/orange/OPEL/8.jpg";
-import opel9 from "../../assets/Images/orange/OPEL/9.jpg";
-import orangeBmw01 from "../../assets/Images/orange/BMW/1.jpg";
-import orangeBmw02 from "../../assets/Images/orange/BMW/2.jpg";
-import orangeBmw03 from "../../assets/Images/orange/BMW/3.jpg";
-import orangeBmw04 from "../../assets/Images/orange/BMW/4.jpg";
+import opel1 from "../../assets/Images/orange/OPEL/1.webp";
+import opel2 from "../../assets/Images/orange/OPEL/2.webp";
+import opel3 from "../../assets/Images/orange/OPEL/3.webp";
+import opel4 from "../../assets/Images/orange/OPEL/4.webp";
+import opel5 from "../../assets/Images/orange/OPEL/5.webp";
+import opel6 from "../../assets/Images/orange/OPEL/6.webp";
+import opel7 from "../../assets/Images/orange/OPEL/7.webp";
+import opel8 from "../../assets/Images/orange/OPEL/8.webp";
+import opel9 from "../../assets/Images/orange/OPEL/9.webp";
+import orangeBmw01 from "../../assets/Images/orange/BMW/1.webp";
+import orangeBmw02 from "../../assets/Images/orange/BMW/2.webp";
+import orangeBmw03 from "../../assets/Images/orange/BMW/3.webp";
+import orangeBmw04 from "../../assets/Images/orange/BMW/4.webp";
 
-import hyndai from "../../assets/Images/orange/4.jpg";
-import vw from "../../assets/Images/orange/7.jpeg";
+import hyndai from "../../assets/Images/orange/4.webp";
+import vw from "../../assets/Images/orange/7.webp";
 
 //**************BLUE IMAGES**************************************
-import Seat from "../../assets/Images/blue/SEAT/1.jpg";
-import Seat2 from "../../assets/Images/blue/SEAT/2.jpg";
-import Seat3 from "../../assets/Images/blue/SEAT/3.jpg";
-import Seat4 from "../../assets/Images/blue/SEAT/4.jpg";
-import honda from "../../assets/Images/blue/HONDA/5.jpg";
-import Hyndai from "../../assets/Images/blue/HYNDAI/6.jpg";
-import blueBmw from "../../assets/Images/blue/BMW/3.jpeg";
-import blueBmw1 from "../../assets/Images/blue/BMW/1.jpg";
-import blueBmw2 from "../../assets/Images/blue/BMW/2.jpg";
-import blueBmw3 from "../../assets/Images/blue/BMW/3.jpg";
-import blueBmw4 from "../../assets/Images/blue/BMW/4.jpg";
-import KIA from "../../assets/Images/blue/KIA/2.jpg";
+import Seat from "../../assets/Images/blue/SEAT/1.webp";
+import Seat2 from "../../assets/Images/blue/SEAT/2.webp";
+import Seat3 from "../../assets/Images/blue/SEAT/3.webp";
+import Seat4 from "../../assets/Images/blue/SEAT/4.webp";
+import honda from "../../assets/Images/blue/HONDA/5.webp";
+import Hyndai from "../../assets/Images/blue/HYNDAI/6.webp";
+import blueBmw from "../../assets/Images/blue/BMW/3.webp";
+import blueBmw1 from "../../assets/Images/blue/BMW/1.webp";
+import blueBmw2 from "../../assets/Images/blue/BMW/2.webp";
+import blueBmw3 from "../../assets/Images/blue/BMW/3.webp";
+import blueBmw4 from "../../assets/Images/blue/BMW/4.webp";
+import blueBmw5 from "../../assets/Images/blue/BMW/5.webp";
+import KIA from "../../assets/Images/blue/KIA/2.webp";
 import vrs from "../../assets/Images/blue/4.jpg";
-import honda01 from "../../assets/Images/blue/HONDA/1.jpeg";
-import Hyndai1 from "../../assets/Images/blue/HYNDAI/1.jpg";
+import honda01 from "../../assets/Images/blue/HONDA/1.webp";
+import Hyndai1 from "../../assets/Images/blue/HYNDAI/1.webp";
 import vrs1 from "../../assets/Images/blue/VRS1.jpg";
 import vrs2 from "../../assets/Images/blue/VRS2.jpg";
 
 //**************YELLOW IMAGES**************************************
-import MIT1 from "../../assets/Images/yellow/MITSUBISHI/1.jpg";
-import MIT2 from "../../assets/Images/yellow/MITSUBISHI/2.jpg";
-import MIT3 from "../../assets/Images/yellow/MITSUBISHI/3.jpg";
-import vw4 from "../../assets/Images/yellow/VW/4.jpg";
-import vw5 from "../../assets/Images/yellow/VW/5.jpg";
-import yellowBmw from "../../assets/Images/yellow/BMW/6.jpg";
-import yellowBmw2 from "../../assets/Images/yellow/BMW/7.jpg";
+import MIT1 from "../../assets/Images/yellow/MITSUBISHI/1.webp";
+import MIT2 from "../../assets/Images/yellow/MITSUBISHI/2.webp";
+import MIT3 from "../../assets/Images/yellow/MITSUBISHI/3.webp";
+import vw4 from "../../assets/Images/yellow/VW/4.webp";
+import vw5 from "../../assets/Images/yellow/VW/5.webp";
+import yellowBmw from "../../assets/Images/yellow/BMW/6.webp";
+import yellowBmw2 from "../../assets/Images/yellow/BMW/7.webp";
 
 //**************PURPLE IMAGES**************************************
-import ibiza1 from "../../assets/Images/purple/SEAT/1.jpg";
-import ibiza2 from "../../assets/Images/purple/SEAT/2.jpg";
-import ibiza3 from "../../assets/Images/purple/SEAT/3.jpg";
-import ibiza4 from "../../assets/Images/purple/SEAT/4.jpg";
-import ibiza5 from "../../assets/Images/purple/SEAT/5.jpg";
-import ibiza6 from "../../assets/Images/purple/SEAT/6.jpg";
-import ibiza7 from "../../assets/Images/purple/SEAT/7.jpg";
-import ibiza8 from "../../assets/Images/purple/SEAT/8.jpg";
-import ibiza9 from "../../assets/Images/purple/SEAT/1.jpg";
-import ibiza10 from "../../assets/Images/purple/SEAT/10.jpg";
-import ibiza11 from "../../assets/Images/purple/SEAT/3.jpg";
-import purpleVw from "../../assets/Images/purple/5.jpg";
-import opel01 from "../../assets/Images/purple/OPEL/6.jpg";
-import opel02 from "../../assets/Images/purple/OPEL/7.jpg";
-import opel03 from "../../assets/Images/purple/OPEL/8.jpg";
-import opel04 from "../../assets/Images/purple/OPEL/9.jpg";
+import ibiza1 from "../../assets/Images/purple/SEAT/1.webp";
+import ibiza2 from "../../assets/Images/purple/SEAT/2.webp";
+import ibiza3 from "../../assets/Images/purple/SEAT/3.webp";
+import ibiza4 from "../../assets/Images/purple/SEAT/4.webp";
+import ibiza5 from "../../assets/Images/purple/SEAT/5.webp";
+import ibiza6 from "../../assets/Images/purple/SEAT/6.webp";
+import ibiza7 from "../../assets/Images/purple/SEAT/7.webp";
+import ibiza8 from "../../assets/Images/purple/SEAT/8.webp";
+import ibiza9 from "../../assets/Images/purple/SEAT/1.webp";
+import ibiza10 from "../../assets/Images/purple/SEAT/10.webp";
+import ibiza11 from "../../assets/Images/purple/SEAT/3.webp";
+import purpleVw from "../../assets/Images/purple/5.webp";
+import opel01 from "../../assets/Images/purple/OPEL/6.webp";
+import opel02 from "../../assets/Images/purple/OPEL/7.webp";
+import opel03 from "../../assets/Images/purple/OPEL/8.webp";
 //**************white IMAGES**************************************
-import whiteVw01 from "../../assets/Images/white/VW/1.jpg";
-import whiteVw02 from "../../assets/Images/white/VW/2.jpg";
-import whiteSeat01 from "../../assets/Images/white/SEAT/3.jpg";
-import whiteSeat02 from "../../assets/Images/white/SEAT/4.jpg";
-import toyota from "../../assets/Images/white/TOYOTA/5.jpg";
-import whiteBmw from "../../assets/Images/white/BMW/6.jpg";
-import HYNDAI from "../../assets/Images/white/HYNDAI/7.jpg";
-import Dayoo from "../../assets/Images/white/DAWOO/8.jpg";
+import whiteVw01 from "../../assets/Images/white/VW/1.webp";
+import whiteVw02 from "../../assets/Images/white/VW/2.webp";
+import whiteSeat01 from "../../assets/Images/white/SEAT/3.webp";
+import whiteSeat02 from "../../assets/Images/white/SEAT/4.webp";
+import toyota from "../../assets/Images/white/TOYOTA/5.webp";
+import whiteBmw from "../../assets/Images/white/BMW/6.webp";
+import HYNDAI from "../../assets/Images/white/HYNDAI/7.webp";
+import Dayoo from "../../assets/Images/white/DAWOO/8.webp";
 
 import slide1 from "../../assets/Images/slide1.jpg";
 import slide2 from "../../assets/Images/slide2.jpg";
@@ -111,13 +110,13 @@ import fordEvent from "../../assets/Images/fordEvent.jpg";
 
 const images = 
 { 
-   BMW1,insgnia1 ,  insgnia2 ,insgnia4,  insgnia3 ,  BMW3,  BMW4,  porscha,  seat,
+   BMW1,insgnia1 ,blueBmw5,  insgnia2 ,insgnia4,  insgnia3 ,  BMW3,  BMW4,  porscha,  seat,
     skoda,  opel1,  opel2,  opel3,  opel4,  opel5,  hyndai,  vw,  Seat,  honda,  Hyndai,  blueBmw,
       KIA,  vrs,  MIT1,  MIT2,  MIT3,  vw4,  vw5,  yellowBmw,  yellowBmw2,  ibiza1,  ibiza2,  ibiza3, 
  ibiza4,  purpleVw,  opel01,  opel02,  opel03,  whiteVw01,  whiteVw02,  whiteSeat01,  whiteSeat02,
    whiteBmw,  HYNDAI,  Dayoo,  toyota,  video,  fordEvent,  slide1,  slide2,  slide3,  slide4,  slide5, 
-    slide6,  E92,  E921,  E922,  MOTO,  MOTO2,  slide8,  honda1,  honda2,  honda3,  puma,  khonfsa,  skoda1,
-      skoda2,skodaa7,ibiza5, ibiza6 ,ibiza7, ibiza8 ,ibiza9 ,ibiza10,ibiza11,opel04,opel6,opel7,opel8,opel9,
+    slide6,  E92,  E921,  E922,  MOTO,  slide8,  honda1,  honda2,  honda3,  puma,  khonfsa,  skoda1,
+      skoda2,skodaa7,ibiza5, ibiza6 ,ibiza7, ibiza8 ,ibiza9 , ibiza10, ibiza11 ,opel6,opel7,opel8,opel9,
       orangeBmw01,orangeBmw02,orangeBmw03,orangeBmw04,Seat2,  Seat3,blueBmw1,  blueBmw2,  blueBmw3,  blueBmw4 ,Seat4,
       honda01,Hyndai1 ,vrs1,vrs2   };
 
